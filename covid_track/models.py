bcb3db1f195c1a9c92e9b4f8cc5c48f34468a792
ssebadduka <seba.jamir@gmail.com>
2021-02-18 15:01:16 +0300

    covid v2

--- covid_track/models.py
+++ covid_track/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import  timezone
+import  datetime
 
 # Create your models here.
+class Patient_info(models.Model):
+    sex = [
+        ('Male','Male'),
+        ('Female','Female')
+    ]
+    date_registered = models.DateTimeField(timezone.now)
+    firstname = models.CharField(max_length=45)
+    lastname = models.CharField(max_length=45)
+    gender = models.CharField(choices=sex,default='Male')
+    Date_of_birth = models.DateTimeField()
+    nationality = models.CharField(max_length= 45)
+    phone = models.CharField(max_length=45)
+    address = models.CharField(max_length=45)
+    district = models.CharField(max_length=45)
+    
+
+
+
